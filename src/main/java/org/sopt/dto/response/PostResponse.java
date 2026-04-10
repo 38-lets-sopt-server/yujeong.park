bb5b0a5bc258be73ac16179fe4f3bc1f312e3019
yujeong <parkyj0430@gmail.com>
2026-04-10 13:57:34 +0900

    feat: dto 코드 작성

---
+++ src/main/java/org/sopt/dto/response/PostResponse.java
@@ -0,0 +1,22 @@
+package org.sopt.dto.response;
+
+public class PostResponse {
+    public Long id;
+    public String title;
+    public String content;
+    public String author;
+    public String createdAt;
+
+    public PostResponse(Long id, String title, String content, String author, String createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + title + " - " + author + " (" + createdAt + ")\n" + content;
+    }
+}
