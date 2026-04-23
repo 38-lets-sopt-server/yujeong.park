79d86a6f5184866dba0996ce9b7e33cda727e229
yujeong <parkyj0430@gmail.com>
2026-04-23 21:15:44 +0900

    refactor: enum 이름 변경, 주석 추가

--- src/main/java/org/sopt/common/CommonErrorCode.java
+++ src/main/java/org/sopt/common/CommonErrorCode.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public enum CommonErrorCode implements ErrorCode {
 
-    INVALID_ARGUMENT(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
--- src/main/java/org/sopt/dto/response/DeletePostResponse.java
+++ src/main/java/org/sopt/dto/response/DeletePostResponse.java
@@ -1,5 +1,6 @@
 package org.sopt.dto.response;
 
+// 게시글 수정 응답
 public record DeletePostResponse(
         Long id
 ) {
